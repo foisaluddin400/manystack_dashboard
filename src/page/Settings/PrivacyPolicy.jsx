4d65c9e0346af3248ff214439deed68b3b65d5ba
foisaluddin400 <foisalrk2@gmail.com>
2025-05-23 17:36:22 +0600

    wer

--- src/page/Settings/PrivacyPolicy.jsx
+++ src/page/Settings/PrivacyPolicy.jsx
@@ -26,7 +26,7 @@ const PrivacyPolicy = () => {
       'align'
     ]
   }
-//sdfsdfsdfsdfsdrgdf
+//sdfsdfsdfsdfsdrgd
   return (
     <div className=" mx-auto ">
       <div className="flex justify-between">
